af98870414541ac4c8ed7dca4254ceb5838a89dc
Daniel Micay <danielmicay@gmail.com>
2019-06-27 23:36:11 -0400

    version 1

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PdfViewer
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := prebuilt/PdfViewer.apk
+
+include $(BUILD_PREBUILT)
