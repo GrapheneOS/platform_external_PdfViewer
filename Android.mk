ac165d8f5674a5536579a14d8f0ac1480077f452
Daniel Micay <danielmicay@gmail.com>
2020-11-25 04:11:39 -0500

    Revert "migrate to Android.bp"
    This reverts commit ff5b226c7d1ad830c0d1af79d4f5b26987d0de14.

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PdfViewer
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := prebuilt/PdfViewer.apk
+LOCAL_PRODUCT_MODULE := true
+
+include $(BUILD_PREBUILT)
